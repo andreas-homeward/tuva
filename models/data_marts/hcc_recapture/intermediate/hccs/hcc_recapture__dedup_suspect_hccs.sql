b9c2b75d1e60cb036fab372b1e9b18d396363a0e
Andreas Martinson <amartinson193@gmail.com>
2026-05-18 10:43:59 -0700

    updating dedup

--- models/data_marts/hcc_recapture/intermediate/hccs/hcc_recapture__dedup_suspect_hccs.sql
+++ models/data_marts/hcc_recapture/intermediate/hccs/hcc_recapture__dedup_suspect_hccs.sql
@@ -4,28 +4,35 @@
 }}
 
 with base as (
-select
-    person_id
-    , payer
-    , data_source
-    , recorded_date
-    , model_version
-    , claim_id
-    , hcc_code
-    , hcc_description
-    , suspect_hcc_flag
-    , eligible_claim_flag
-    , hcc_type
-    , hcc_source
+    select
+        person_id
+        , payer
+        , data_source
+        , {{ date_part('year', 'sus.recorded_date') }} as collection_year
+        , recorded_date
+        , model_version
+        , claim_id
+        , hcc_code
+        , hcc_description
+        , suspect_hcc_flag
+        , eligible_claim_flag
+        , hcc_type
+        , hcc_source
+    from {{ ref('hcc_recapture__int_suspect_hccs')}}
+)
+
+, add_rank as (
+    select 
+        * 
     -- Ensure only 1 hcc type per HCC
     , rank() over (
-        partition by person_id, payer, data_source, claim_id, hcc_code, model_version 
+        partition by person_id, payer, claim_id, hcc_code, model_version, collection_year
             order by case 
                         when hcc_type = 'captured' then 1
                         when hcc_type = 'suspect' then 2
                      end
-    ) as hcc_type_rank
-from {{ ref('hcc_recapture__int_suspect_hccs')}}
+    ) as hcc_type_rank      
+    from base  
 )
 
 select 
