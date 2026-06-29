d727c064a143a798b8a03927a01de71e3ce924a9
Andreas Martinson <amartinson193@gmail.com>
2026-06-29 08:39:51 -0700

    fixing bug with duplicate as

--- models/data_marts/hcc_recapture/staging/hcc_recapture__stg_suspect_hccs.sql
+++ models/data_marts/hcc_recapture/staging/hcc_recapture__stg_suspect_hccs.sql
@@ -12,7 +12,7 @@ select
     , cast(claim_id as {{dbt.type_string()}}) as claim_id
     , cast(hcc_code as {{dbt.type_string()}}) as hcc_code
     , cast(hcc_description as {{dbt.type_string()}}) as hcc_description
-    , cast(reason as as {{dbt.type_string()}}) as reason
+    , cast(reason as {{dbt.type_string()}}) as reason
     , cast(hcc_type as {{dbt.type_string()}}) as hcc_type
     , cast(hcc_source as {{dbt.type_string()}}) as hcc_source
 from {{ ref('suspect_hccs')}}
